7e8c14bf906b7f5567eff7790797b72d5806b2a6
NiyaziPro <niyazicholak@gmail.com>
2025-05-08 19:33:52 +0200

    19:33 Thursday 08.05.2025

--- tests/day02/01-practice-user-facing-locators.spec.ts
+++ tests/day02/01-practice-user-facing-locators.spec.ts
@@ -9,4 +9,36 @@ test("Practice 1 - User Facing Locators", async ({ page }) => {
   await page.getByRole("button", { name: "Login" }).click();
   await expect(page.getByRole("heading", { name: "Dashboard" })).toBeVisible();
   await page.getByRole("link", { name: "My Info" }).click();
+  await expect(
+    page.getByRole("heading", { name: "Personal Details" })
+  ).toBeVisible();
+});
+
+test('Practice 2 - User Facing Locators', async ({ page }) => {
+    await page.goto('https://saucedemo.com/');
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page.getByTestId('title')).toBeVisible();
+    await expect(page.locator('.app_logo')).toContainText('Swag Labs');
+    await page.getByTestId('add-to-cart-sauce-labs-backpack').click();
+    await page.getByTestId('item-0-title-link').click();
+    
+    await page.getByTestId('add-to-cart').click();
+    await page.getByTestId('remove').click();
+    await page.getByTestId('shopping-cart-link').click();
+    await page.getByTestId('checkout').click();
+    await page.getByTestId('cancel').click();
+    await page.getByTestId('continue-shopping').click();
+});
+
+test('Practice 3', async ({ page }) => {
+    await page.goto('https://magento.softwaretestingboard.com/', { waitUntil: 'load' });
+    await page.getByRole('button', { name: 'DISAGREE' }).click();
+    await page.getByPlaceholder('Search entire store here...').fill('t-shirt');
+    await page.getByTitle('Search').click();
+
+    await page.getByAltText('Balboa Persistence Tee').click();
+    await expect(page.getByRole('heading', { name: 'Balboa Persistence Tee' })).toBeVisible();
+   
 });
