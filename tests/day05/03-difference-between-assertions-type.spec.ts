e9ee405ab9cdcb0a8a959557451d256dade33d7a
NiyaziPro <niyazicholak@gmail.com>
2025-05-27 21:15:08 +0200

    21:15 Tuesday 27.05.2025

--- tests/day05/03-difference-between-assertions-type.spec.ts
+++ tests/day05/03-difference-between-assertions-type.spec.ts
@@ -16,3 +16,25 @@ test("Difference between assertion types", async ({ page }) => {
   const buttonClassValue = await buttonLocator.getAttribute("class"); // --> mt-4 btn btn-primary
   expect(buttonClassValue).toEqual("mt-4 text-danger btn btn-primary"); // Non-retrying assertion <==FAIL
 });
+
+test('Difference between assertion types - 2', async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/dynamic_controls");
+
+    // Wait for the page to load
+    await page.waitForLoadState("networkidle");
+
+    // Locate the checkbox
+    const checkboxLocator = page.locator("#checkbox-example").getByRole("button");
+    await checkboxLocator.click();
+
+    // Auto retrying assertion
+    await expect(checkboxLocator).toHaveText("Add"); // Auto retrying assertion
+    // Wait for the checkbox to be removed
+    await page.waitForSelector("#checkbox", { state: "detached" });
+
+
+    // Non-retrying assertion
+    const checkboxText = await checkboxLocator.textContent(); // <-- Removes the checkbox
+    expect(checkboxText).toEqual("Add"); // Non-retrying assertion <==FAIL
+
+});
