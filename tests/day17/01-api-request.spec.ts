340060fd5579ab07a9cb9d8e5235c23feab38c6a
NiyaziPro <niyazicholak@gmail.com>
2025-06-14 15:40:25 +0200

    15:40 Saturday 14.06.2025

--- tests/day17/01-api-request.spec.ts
+++ tests/day17/01-api-request.spec.ts
@@ -1,7 +1,52 @@
 import { test, expect } from "@playwright/test";
-import postData from "../../data/petPostData.json"
+import postData from "../../data/petPostData.json";
 
-test("POST request - Pet Store", async ({ request }) => { 
+test.describe("Pet Store API Suite", () => {
+  test.describe.configure({ mode: "serial" });
+  let petId;
+  test("POST request - Pet Store", async ({ request }) => {
+    const response = await request.post("https://petstore.swagger.io/v2/pet", {
+      data: postData,
+    });
+    const responseData = await response.json();
+    console.log(responseData);
+    petId = responseData.id;
 
+    expect(response.status()).toBe(200);
+    expect(responseData.category.id).toBe(123);
+    expect(responseData.category.name).toEqual("Dog");
+    expect(responseData.name).toEqual("Lasi");
+    expect(responseData.tags[0].name).toEqual("Samoed");
+  });
 
+  test("GET request - Pet Store", async ({ request }) => {
+    const response = await request.get(
+      "https://petstore.swagger.io/v2/pet/" + petId
+    );
+    const responseData = await response.json();
+    console.log(responseData);
+    expect(response.status()).toBe(200);
+    expect(responseData.category.id).toBe(123);
+    expect(responseData.category.name).toEqual("Dog");
+    expect(responseData.name).toEqual("Lasi");
+    expect(responseData.tags[0].name).toEqual("Samoed");
+  });
+
+  test("DELETE request - Pet Store", async ({ request }) => {
+    const response = await request.delete(
+      "https://petstore.swagger.io/v2/pet/" + petId
+    );
+    const responseData = await response.json();
+    console.log(responseData);
+    expect(response.status()).toBe(200);
+    expect(responseData.type).toEqual("unknown");
+    expect(responseData.message).toEqual(petId.toString());
+  });
+
+  test("GET request after DELETE- Pet Store", async ({ request }) => {
+    const response = await request.get(
+      "https://petstore.swagger.io/v2/pet/" + petId
+    );
+    expect(response.status()).toBe(404);
+  });
 });
